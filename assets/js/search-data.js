01dc7bf52fe8933995ce03d757174500bca69d44
zhangdian617 <58057130+zhangdian617@users.noreply.github.com>
2026-01-26 01:48:34 +0000

    Deploying to gh-pages from @ zhangdian617/academic@15561089569217f964dcdd281296a19e3ca57e06 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -579,7 +579,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=tiE0IDMAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
